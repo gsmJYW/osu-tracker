392b890acdc01b8317a18371c6f180085b205473
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2020-11-02 15:57:51 +0900

    경로 변경, 스타레이팅 추가

--- api/Beatmap.cs
+++ api/Beatmap.cs
@@ -1,11 +1,12 @@
-﻿namespace osu_tracker
+﻿namespace osu_tracker.api
 {
-    class MapInfo
+    class Beatmap
     {
         public string artist { get; set; }
         public int beatmapset_id { get; set; }
         public string title { get; set; }
         public string version { get; set; }
         public int max_combo { get; set; }
+        public double difficultyrating { get; set; }
     }
 }
