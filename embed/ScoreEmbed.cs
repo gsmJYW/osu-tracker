248b701c2eb2c017bf3680927ec71e48faddbe0d
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2020-11-02 13:38:56 +0900

    경로 변경

---
+++ embed/ScoreEmbed.cs
@@ -0,0 +1,48 @@
+﻿using Discord;
+using osu_tracker.api;
+
+namespace osu_tracker.embed
+{
+    class ScoreEmbed : EmbedBuilder
+    {
+        public ScoreEmbed(Score score)
+        {
+            User user = OsuConvert.ToUser(score.user_id);
+            Beatmap beatmap = OsuConvert.ToBeatmap(score.beatmap_id, score.enabled_mods);
+
+            string modsString = OsuConvert.ToModString(score.enabled_mods);
+            if (modsString.Length > 0)
+            {
+                modsString = " +" + modsString;
+            }
+
+            WithTitle(string.Format("\n{0} - {1}", beatmap.artist, beatmap.title));
+            WithDescription(string.Format("**[{0}]{1} {2:0.##}🟊**\n​", beatmap.version, modsString, beatmap.difficultyrating));
+            WithUrl(string.Format("https://osu.ppy.sh/beatmapsets/{0}#osu/{1}", beatmap.beatmapset_id, score.beatmap_id));
+            WithColor(new Color(0xFF69B4));
+            WithTimestamp(OsuConvert.ToOffset(score.date));
+            WithFooter(footer => { footer
+                    .WithText((score.index + 1) + "번째 탑 플레이");
+            });
+            WithThumbnailUrl(score.RankImageUrl());
+            WithImageUrl(string.Format("https://assets.ppy.sh/beatmaps/{0}/covers/cover.jpg", beatmap.beatmapset_id));
+            WithAuthor(author => { author
+                .WithName(user.username)
+                .WithUrl("https://osu.ppy.sh/users/" + user.user_id)
+                .WithIconUrl("https://github.com/ppy/osu/blob/master/assets/lazer.png?raw=true");
+            });
+            AddField("점수", score.score, true);
+            AddField("퍼포먼스", string.Format("{0:0.##}pp", score.pp), true);
+            AddField("\u200B", "\u200B", true);
+            AddField("콤보", string.Format("{0}/{1}", score.maxcombo, beatmap.max_combo), true);
+            AddField("정확도", string.Format("{0:0.##}%", score.Accuracy()), true);
+            AddField("\u200B", "\u200B", true);
+            AddField("300", "x" + score.count300, true);
+            AddField("100", "x" + score.count100, true);
+            AddField("\u200B", "\u200B", true);
+            AddField("50", "x" + score.count50, true);
+            AddField("미스", "x" + score.countmiss, true);
+            AddField("\u200B", "\u200B", true);
+        }
+    }
+}
---
+++ embed/UserEmbed.cs
@@ -0,0 +1,58 @@
+﻿using Discord;
+using osu_tracker.api;
+
+namespace osu_tracker.embed
+{
+    class UserEmbed : EmbedBuilder
+    {
+        public UserEmbed(User user)
+        {
+            UserBest userBest = new UserBest(user.user_id);
+            string ppString, ppRankString, ppCountryRankString, modString = Get.ModString(userBest.MainMods());
+
+            if (modString.Length == 0)
+            {
+                modString = "노모드";
+            }
+
+            // inactive 플레이어
+            if (user.pp_raw == 0)
+            {
+                ppString = "활동 없음";
+                ppRankString = "활동 없음";
+                ppCountryRankString = "활동 없음";
+            }
+            else
+            {
+                ppString = string.Format("{0:0.##}pp", user.pp_raw);
+                ppRankString = "#" + user.pp_rank;
+                ppCountryRankString = "#" + user.pp_country_rank;
+            }
+
+            WithColor(new Color(0xFF69B4));
+            WithDescription("​");
+            WithTimestamp(Get.OffsetByDate(user.join_date));
+            WithFooter(footer => { footer
+                .WithText("가입 일시");
+            });
+            WithThumbnailUrl("https://a.ppy.sh/" + user.user_id);
+            WithAuthor(author => { author
+                .WithName(user.username)
+                .WithUrl("https://osu.ppy.sh/users/" + user.user_id)
+                .WithIconUrl("https://github.com/ppy/osu/blob/master/assets/lazer.png?raw=true");
+            });
+            AddField("퍼포먼스", ppString, true);
+            AddField("주력 모드", modString, true);
+            AddField("\u200B", "\u200B", true);
+            AddField("순위", ppRankString, true);
+            AddField("국가 순위", string.Format(":flag_{0}: {1}", user.country.ToLower(), ppCountryRankString), true);
+            AddField("\u200B", "\u200B", true);
+            AddField("정확도", string.Format("{0:0.##}%", user.accuracy), true);
+            AddField("레벨", string.Format("{0:0.##}", user.level), true);
+            AddField("\u200B", "\u200B", true);
+            AddField("플레이 횟수", user.playcount + "회", true);
+            AddField("플레이 시간", user.total_seconds_played / 3600 + "시간", true);
+            AddField("\u200B", "\u200B", true);
+        }
+    }
+}
