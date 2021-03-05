030f943b2d2a0ab845fdb7017c751b5875c642ed
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-03-05 11:00:42 +0900

    feat: 플레이 기록이 없는 유저, 장기간 플레이하지 않은 유저를 구분해서 출력하도록 변경

--- embed/UserEmbed.cs
+++ embed/UserEmbed.cs
@@ -7,52 +7,81 @@ namespace osu_tracker.embed
     {
         public UserEmbed(User user)
         {
-            UserBest userBest = new UserBest(user.user_id);
-            string ppString, ppRankString, ppCountryRankString, modString = OsuConvert.ToModString(userBest.MainMods());
+            string mainModString;
 
-            if (modString.Length == 0)
+            if (user.pp_rank == 0)
             {
-                modString = "노모드";
-            }
-
-            // inactive 플레이어
-            if (user.pp_raw == 0)
-            {
-                ppString = "활동 없음";
-                ppRankString = "활동 없음";
-                ppCountryRankString = "활동 없음";
+                mainModString = "없음";
             }
             else
             {
-                ppString = string.Format("{0:0.##}pp", user.pp_raw);
-                ppRankString = "#" + user.pp_rank;
-                ppCountryRankString = "#" + user.pp_country_rank;
+                UserBest userBest = new UserBest(user.user_id);
+                userBest.GetMainMods();
+
+                if (userBest.mainMods == 0)
+                {
+                    mainModString = "노모드";
+                }
+                else
+                {
+                    mainModString = userBest.mainMods.ToModString();
+                }
             }
 
+            if (user.pp_rank == 0)
+            {
+                WithDescription("플레이 ​기록이 없는 유저\n\u200B");
+                AddField("레벨", string.Format("{0:0.##}", user.level), true);
+                AddField("\u200B", "\u200B", true);
+            }
+            else if (user.pp_raw == 0)
+            {
+                WithDescription("​장기간 활동이 없는 유저\n\u200B");
+
+                AddField("순위", "#" + user.pp_rank, true);
+                AddField("주력 모드", mainModString, true);
+                AddField("\u200B", "\u200B", true);
+                AddField("정확도", string.Format("{0:0.##}%", user.accuracy), true);
+                AddField("레벨", string.Format("{0:0.##}", user.level), true);
+            }
+            else
+            {
+                if (user.user_id == 10901226)
+                {
+                    WithDescription("개발자입니다!\n\u200B");
+                }
+                else
+                {
+                    WithDescription("​");
+                }
+
+                AddField("퍼포먼스", string.Format("{0:0.##}pp", user.pp_raw), true);
+                AddField("주력 모드", mainModString, true);
+                AddField("\u200B", "\u200B", true);
+                AddField("순위", "#" + user.pp_rank, true);
+                AddField("국가 순위", "#" + user.pp_country_rank, true);
+                AddField("\u200B", "\u200B", true);
+                AddField("정확도", string.Format("{0:0.##}%", user.accuracy), true);
+                AddField("레벨", string.Format("{0:0.##}", user.level), true);
+            }
+
+            AddField("\u200B", "\u200B", true);
+            AddField("플레이 횟수", user.playcount + "회\n\u200B", true);
+            AddField("플레이 시간", user.total_seconds_played / 3600 + "시간\u200B", true);
+            AddField("\u200B", "\u200B", true);
+
             WithColor(new Color(0xFF69B4));
-            WithDescription("​");
-            WithTimestamp(OsuConvert.ToDateTimeOffset(user.join_date));
+            WithTimestamp(user.join_date.ToDateTimeOffset());
             WithFooter(footer => { footer
                 .WithText("가입 일시");
             });
+
             WithThumbnailUrl("https://a.ppy.sh/" + user.user_id);
             WithAuthor(author => { author
                 .WithName(user.username)
                 .WithUrl("https://osu.ppy.sh/users/" + user.user_id)
-                .WithIconUrl("https://github.com/ppy/osu/blob/master/assets/lazer.png?raw=true");
+                .WithIconUrl("https://www.countryflags.io/" + user.country.ToLower() + "/flat/64.png");
             });
-            AddField("퍼포먼스", ppString, true);
-            AddField("주력 모드", modString, true);
-            AddField("\u200B", "\u200B", true);
-            AddField("순위", ppRankString, true);
-            AddField("국가 순위", string.Format(":flag_{0}: {1}", user.country.ToLower(), ppCountryRankString), true);
-            AddField("\u200B", "\u200B", true);
-            AddField("정확도", string.Format("{0:0.##}%", user.accuracy), true);
-            AddField("레벨", string.Format("{0:0.##}", user.level), true);
-            AddField("\u200B", "\u200B", true);
-            AddField("플레이 횟수", user.playcount + "회", true);
-            AddField("플레이 시간", user.total_seconds_played / 3600 + "시간", true);
-            AddField("\u200B", "\u200B", true);
         }
     }
 }
