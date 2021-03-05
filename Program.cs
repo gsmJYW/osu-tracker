b7932685edf8ba7509b3d77520019b2e4500a3dd
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-03-05 10:42:00 +0900

    feat: 프로그램 환경설정을 main 매개변수로 받도록 변경, 추적 결과를 유저가 지정한 채널로 보내지 않고 osu-tracker 채널을 생성해 보내도록 변경

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
-using System.IO;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using Discord.Rest;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using osu_tracker.api;
@@ -20,22 +21,42 @@ namespace osu_tracker
         private CommandService _commands;
         private IServiceProvider _services;
 
+        public static string api_key, bot_token, mysql_server, mysql_port, mysql_database, mysql_uid, mysql_password, mysql_charset;
+
         static void Main(string[] args)
         {
             try
             {
-                new Config();
+                api_key = args[0];
+                bot_token = args[1];
+
+                mysql_server = args[2];
+                mysql_port = args[3];
+                mysql_database = args[4];
+                mysql_uid = args[5];
+                mysql_password = args[6];
             }
-            catch (Exception e)
+            catch
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine("프로그램 실행 시 다음 매개변수가 필요합니다:\n" +
+                    "[osu!API v1 키] [디스코드 봇 토큰] [MySQL 서버] [MySQL 포트] [MySQL DB] [MySQL 유저] [MySQL 비밀번호] <MySQL 인코딩>"
+                    );
                 return;
             }
-            
+
+            try
+            {
+                mysql_charset = args[7];
+            }
+            catch
+            {
+                mysql_charset = "UTF8";
+            }
+
             // mysql 연결 시도
             try
             {
-                new Sql();
+                new Sql(mysql_server, mysql_port, mysql_database, mysql_uid, mysql_password, mysql_charset);
             }
             // 실패 시 종료
             catch (Exception e)
@@ -52,47 +73,94 @@ namespace osu_tracker
 
         public async Task CheckNewBest()
         {
-            DataTable users = Sql.Get("SELECT user_id FROM targets GROUP BY user_id");
+            DataTable userTable = Sql.Get("SELECT user_id FROM targets GROUP BY user_id");
 
             // 타겟 유저 마다 검사
-            foreach (DataRow user in users.Rows)
+            foreach (DataRow userRow in userTable.Rows)
             {
                 try
                 {
-                    int user_id = (int)user["user_id"];
+                    UserBest userBest;
+                    User user = User.Search((int)userRow["user_id"]);
 
-                    UserBest userBest = new UserBest(user_id);
-                    Score newBest = userBest.NewBest();
+                    // 이전 pp 기록이 있는지 확인
+                    DataTable ppHistorySearchTable = Sql.Get(
+                        "SELECT p.user_id FROM pphistories p, targets t " +
+                        "WHERE p.user_id = {0} AND p.user_id = t.user_id", user.user_id
+                        );
 
-                    // 새로운 베퍼포가 있을 경우
-                    if (newBest!= null)
+                    // 이전 pp 기록이 없을 경우 새로 삽입하고 다음 타겟 검사
+                    if (ppHistorySearchTable.Rows.Count == 0)
                     {
-                        ScoreEmbed scoreInfoEmbed = new ScoreEmbed(newBest);
-                        DataTable channels = Sql.Get("SELECT guild_id, channel_id FROM targets WHERE user_id = '{0}'", user_id);
+                        userBest = new UserBest(user.user_id);
+                        Sql.Execute("INSERT INTO pphistories VALUES ({0}, {1}, {2}, {3})", user.user_id, userBest.pp_sum, user.pp_raw, user.pp_rank);
 
-                        foreach (DataRow channel in channels.Rows)
+                        continue;
+                    }
+
+                    double previous_pp_raw = Convert.ToDouble(Sql.Get("SELECT pp_raw FROM pphistories WHERE user_id = {0}", user.user_id).Rows[0]["pp_raw"]);
+
+                    // pp 변화가 없을 경우 다음 타겟 검사
+                    if (previous_pp_raw.IsCloseTo(user.pp_raw))
+                    {
+                        continue;
+                    }
+
+                    // pp 변화가 있을 경우 새로운 베퍼포가 있는지 확인
+                    userBest = new UserBest(user.user_id);
+                    userBest.GetNewBest();
+
+                    // 새로운 베퍼포가 없을 경우 다음 타겟 검사
+                    if (userBest.newBest == null)
+                    {
+                        continue;
+                    }
+
+                    ScoreEmbed scoreEmbed = new ScoreEmbed(userBest);
+                    DataTable guildTable = Sql.Get("SELECT guild_id FROM targets WHERE user_id = '{0}'", user.user_id);
+
+                    foreach (DataRow guildRow in guildTable.Rows)
+                    {
+                        ulong guild_id = ulong.Parse(guildRow["guild_id"].ToString());
+                        SocketGuild guild = _client.GetGuild(guild_id);
+
+                        // 'osu!tracker' 채널을 탐색해서 있으면 거기로 전송, 없으면 채널 생성 후 전송
+                        try
                         {
-                            ulong guild_id = ulong.Parse(channel["guild_id"].ToString());
-                            ulong channel_id = ulong.Parse(channel["channel_id"].ToString());
+                            SocketTextChannel channelToSend = null;
+                            bool isThereOsuTrackerChannel = false;
 
-                            // 스코어 정보 전송
-                            try
+                            IReadOnlyCollection<SocketTextChannel> channelList = guild.TextChannels;
+
+                            foreach (SocketTextChannel channel in channelList)
                             {
-                                var socketTextChannel = _client.GetGuild(guild_id).GetTextChannel(channel_id);
-                                await socketTextChannel.SendMessageAsync(embed: scoreInfoEmbed.Build());
-                            }
-                            // 실패 시 채널이 삭제된 것으로 판단하고 DB에서 삭제
-                            catch (NullReferenceException)
-                            {
-                                Sql.Execute("DELETE FROM targets WHERE channel_id = '{0}'", channel_id);
-
-                                // 해당 서버에서 삭제한 유저가 타겟에 더 이상 없을 경우 점수 기록도 삭제
-                                DataTable findUser = Sql.Get("SELECT user_id FROM targets WHERE user_id = {0}", user_id);
-
-                                if (findUser.Rows.Count == 0)
+                                if (channel.Name.ToLower() == "osu-tracker")
                                 {
-                                    Sql.Execute("DELETE FROM pphistories WHERE user_id = {0}", user_id);
+                                    isThereOsuTrackerChannel = true;
+                                    channelToSend = channel;
+                                    break;
                                 }
+                            }
+
+                            if (!isThereOsuTrackerChannel)
+                            {
+                                RestTextChannel CreatedChannel = (await guild.CreateTextChannelAsync("osu-tracker"));
+                                channelToSend = guild.GetTextChannel(CreatedChannel.Id);
+                            }
+
+                            await channelToSend.SendMessageAsync(embed: scoreEmbed.Build());
+                        }
+                        // 실패 시 채널이 삭제된 것으로 판단하고 DB에서 삭제
+                        catch
+                        {
+                            Sql.Execute("DELETE FROM targets WHERE guild_id = '{0}'", guild_id);
+
+                            // 해당 서버에서 삭제한 유저가 타겟에 더 이상 없을 경우 점수 기록도 삭제
+                            DataTable targetSearchTable = Sql.Get("SELECT user_id FROM targets WHERE user_id = {0}", user.user_id);
+
+                            if (targetSearchTable.Rows.Count == 0)
+                            {
+                                Sql.Execute("DELETE FROM pphistories WHERE user_id = {0}", user.user_id);
                             }
                         }
                     }
@@ -118,8 +186,11 @@ namespace osu_tracker
             _client.Log += _client_Log;
 
             await RegisterCommandsAsync();
-            await _client.LoginAsync(TokenType.Bot, Config.bot_token);
+            await _client.LoginAsync(TokenType.Bot, bot_token);
             await _client.StartAsync();
+            
+            Thread.Sleep(5000);
+
             await CheckNewBest();
             await Task.Delay(-1);
         }
