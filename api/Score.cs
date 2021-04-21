9dfc7bef48e741d52457ddc05008fce195b0d18e
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-04-21 20:37:44 +0900

    feat: 불필요해진 RankImageUrl() 함수 제거

--- api/Score.cs
+++ api/Score.cs
@@ -10,9 +10,11 @@ namespace osu_tracker.api
     {
         public int beatmap_id { get; set; }
         public int maxcombo { get; set; }
-        public int count50 { get; set; }
-        public int count100 { get; set; }
         public int count300 { get; set; }
+        public int countgeki { get; set; }
+        public int count100 { get; set; }
+        public int countkatu { get; set; }
+        public int count50 { get; set; }
         public int countmiss { get; set; }
         public int enabled_mods { get; set; }
         public int user_id { get; set; }
@@ -26,41 +28,6 @@ namespace osu_tracker.api
         public double Accuracy()
         {
             return (50.0 * count50 + 100.0 * count100 + 300.0 * count300) / (300.0 * (countmiss + count50 + count100 + count300)) * 100.0;
-        }
-
-        // 랭크에 해당하는 이미지 url
-        public string RankImageUrl()
-        {
-            switch (rank)
-            {
-                case "XH":
-                    return "https://imgur.com/UzDVLCF.png";
-
-                case "X":
-                    return "https://imgur.com/7hMmYYW.png";
-
-                case "SH":
-                    return "https://imgur.com/AMT9Iyy.png";
-
-                case "S":
-                    return "https://imgur.com/NXxWkeX.png";
-
-                case "A":
-                    return "https://imgur.com/QCzekYf.png";
-
-                case "B":
-                    return "https://imgur.com/xq2Q4wB.png";
-
-                case "C":
-                    return "https://imgur.com/fL372Ks.png";
-
-                case "D":
-                    return "https://imgur.com/Dxlcytu.png";
-
-                // F (페일)
-                default:
-                    return "https://imgur.com/C0FLjUs.png";
-            }
         }
 
         public static Score UserRecent(object username)
