5c5f961d0607449decac8a098456922808c4aa27
gsmJYW <49026858+gsmJYW@users.noreply.github.com>
2021-05-10 17:47:25 +0900

    feat: me 명령어 설명 추가

--- src/embed/HelpEmbed.cs
+++ src/embed/HelpEmbed.cs
@@ -7,17 +7,23 @@ namespace osu_tracker.embed
         public HelpEmbed(string avatarUrl)
         {
             WithColor(new Color(0x527788));
-            
             WithThumbnailUrl(avatarUrl);
-            
+
             AddField(">help", "osu!tracker의 사용법을 알려줍니다.");
-            AddField(">info 유저명/유저id", "플레이어의 정보를 보여줍니다.");
-            AddField(">track 유저명/유저id",
-                    "이 서버에서 추적할 플레이어를 추가합니다.\n" +
-                    "플레이어가 자신의 상위 pp 기록을 갱신할 때마다\n" +
-                    "해당 스코어 정보를 #osu-tracker 채널에 보냅니다.\n" +
-                    "이미 추적 중인 플레이어일 경우 추적을 중지합니다.");
-            AddField(">recent 유저명/유저id", "플레이어의 최근 플레이 기록을 보여줍니다.");
+            AddField(">info 유저명", "플레이어의 정보를 보여줍니다.");
+            AddField(">track 유저명",
+                "이 서버에서 추적할 플레이어를 추가합니다.\n" +
+                "플레이어가 자신의 상위 pp 기록을 갱신할 때마다\n" +
+                "해당 스코어 정보를 #osu-tracker 채널에 보냅니다.\n" +
+                "이미 추적 중인 플레이어를 그만 추적하려면\n" +
+                "해당 플레이어의 유저명을 입력하세요.");
+            AddField(">recent 유저명", "플레이어의 최근 플레이 기록을 보여줍니다.");
+            AddField(">me 유저명",
+                "자신의 플레이어 정보를 등록합니다.\n" +
+                "등록함으로써 다른 명령어들을 사용할 때\n" +
+                "유저명을 생략할 수 있습니다.\n" +
+                "이미 등록한 플레이어 정보를 삭제하려면\n" +
+                "유저명을 쓰지 말고 `>me`를 입력하세요.");
             AddField(">list", "이 서버에서 추적 중인 플레이어들을 보여줍니다.");
         }
     }
